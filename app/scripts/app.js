44c8b853a20bc8e22593a9d7b4f939fb81879631
Vadim Novash <novash@tut.by>
2016-11-29 20:50:12 +0300

    index page + animation + tariffs + stations page

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,6 +1,14 @@
 import $ from 'jquery';
 import svg4everybody from 'svg4everybody';
+import *as owlCarouse from '../blocks/reviews/owl.carousel';
 
 $(() => {
 	svg4everybody();
+	$('.owl-carousel').owlCarousel({
+		items: 3,
+		autoPlay: true,
+		stopOnHover: true,
+		pagination: true
+
+	});
 });
