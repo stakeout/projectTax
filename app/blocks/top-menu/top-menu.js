861c6186acb6653f01e211aaa6c86b39a1bb3ec2
Vadim Novash <novash@tut.by>
2017-01-11 16:38:31 +0300

    region-tariffs-table + liveinternet counter + mobile menu upgrade

--- app/blocks/top-menu/top-menu.js
+++ app/blocks/top-menu/top-menu.js
@@ -3,8 +3,18 @@ export default () => {
 	const navToggle = document.querySelector('.top-menu__toggle');
 	const menu = $('.top-menu__list');
 	const form = $('#sedi-orderform2');
+	// $('.top-menu__item').on('mouseover', function(){
+	// 	$(this).find(' > ul').css('display', 'block');
+	// }
 	navMain.classList.remove('top-menu_no-js');
 	if (window.matchMedia('(max-width: 380px)').matches) {
+		// $('.sub-menu').classList.add('hidden');
+		$('li.has-child').append('<div class="accordion-toggle"></div>');
+		$('.accordion-toggle').click(function () {
+			$(this).parent().find('> ul').slideToggle(200);
+			// $(this).toggleClass('toggle-background');
+			// $(this).toggleClass('toggle-rotate');
+		});
 		navToggle.addEventListener('click', function () {
 			if (navMain.classList.contains('top-menu_closed')) {
 				navMain.classList.remove('top-menu_closed');
