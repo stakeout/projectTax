81405492aa5cf13a546ea569aba521b565e9500e
Vadim Novash <novash@tut.by>
2017-02-26 17:24:36 +0300

    new navigation structure with dropdowns & JS, without styles

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -2,31 +2,30 @@ import $ from 'jquery';
 import svg4everybody from 'svg4everybody';
 // import *as owlCarouse from '../blocks/reviews/owl.carousel';
 import slick from 'slick-carousel';
-import mainNav from '../blocks/top-menu/top-menu';
+import '../blocks/top-menu/top-menu';
 
 $(() => {
 	svg4everybody();
-	mainNav();
+	$('#navigation').navigation();
 	$('.js-slick').slick({
 		dots: true,
 		arrows: true,
 		slidesToShow: 4,
 		slidesToScroll: 2,
-		responsive: [
-			{
-				breakpoint: 800,
-				settings: {
-					slidesToShow: 3,
-					slidesToScroll: 1
-				}
-			},
-			{
-				breakpoint: 320,
-				settings: {
-					slidesToShow: 1,
-					slidesToScroll: 1
-				}
+		responsive: [{
+			breakpoint: 800,
+			settings: {
+				slidesToShow: 3,
+				slidesToScroll: 1
 			}
+		},
+		{
+			breakpoint: 320,
+			settings: {
+				slidesToShow: 1,
+				slidesToScroll: 1
+			}
+		}
 		]
 	});
 	// $('.owl-carousel').owlCarousel({
